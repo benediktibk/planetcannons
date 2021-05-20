c6f0d114c43ad338e9a92eb3191d7d2d00ca5ab3
Benedikt Schmidt <benediktibk@gmail.com>
2021-05-20 20:37:54 +0200

    clean up code

--- GraphicObjectTriangle.cpp
+++ GraphicObjectTriangle.cpp
@@ -13,18 +13,14 @@ GraphicObjectTriangle::GraphicObjectTriangle(const std::tuple<float, float, floa
     glGenVertexArrays(1, &m_vertexArray);
     glBindVertexArray(m_vertexArray);
 
-    // An array of 3 vectors which represents 3 vertices
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f, -1.0f, 0.0f,
         1.0f, -1.0f, 0.0f,
         0.0f,  1.0f, 0.0f,
     };
 
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
     glGenBuffers(1, &m_vertexBuffer);
-    // The following commands will talk about our 'vertexbuffer' buffer
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
-    // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 }
 
@@ -46,18 +42,9 @@ void GraphicObjectTriangle::setPoints(const std::tuple<float, float, float> &poi
 }
 
 void GraphicObjectTriangle::update() const {
-    // 1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
-    glVertexAttribPointer(
-    0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-    3,                  // size
-    GL_FLOAT,           // type
-    GL_FALSE,           // normalized?
-    0,                  // stride
-    (void*)0            // array buffer offset
-    );
-    // Draw the triangle !
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
 }
