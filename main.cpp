63b8e0d0358f4aa7c344b9bb88bd1db9e0e163d1
Benedikt Schmidt <benediktibk@gmail.com>
2021-05-21 15:10:19 +0200

    add second triangle

--- main.cpp
+++ main.cpp
@@ -8,8 +8,13 @@ int main() {
 	GraphicObjectTriangle *triangleOne = new GraphicObjectTriangle(
 		std::make_tuple(-1.0f, -1.0f, 0.0f), 
 		std::make_tuple(1.0f, -1.0f, 0.0f), 
+		std::make_tuple(0.0f, 0.0f, 0.0f));
+	GraphicObjectTriangle *triangleTwo = new GraphicObjectTriangle(
+		std::make_tuple(-1.0f, 0.0f, 0.0f), 
+		std::make_tuple(1.0f, 0.0f, 0.0f), 
 		std::make_tuple(0.0f, 1.0f, 0.0f));
 	graphicSystem.add(triangleOne);
+	graphicSystem.add(triangleTwo);
 
 	while (!graphicSystem.closeRequested()) {
 		graphicSystem.update();
