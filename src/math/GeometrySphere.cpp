ab47f21ef5ee5975d2e2cdda1e4b0b3a0cfd1e39
Benedikt Schmidt <benediktibk@gmail.com>
2021-05-24 20:52:55 +0200

    remove faulty sphere function

--- src/math/GeometrySphere.cpp
+++ src/math/GeometrySphere.cpp
@@ -12,30 +12,3 @@ LinearAlgebraVector GeometrySphere::calculatePoint(double theta, double phi) con
     auto z = m_center.getZ() + m_radius * cos(theta);
     return LinearAlgebraVector(x, y, z);
 }
-
-LinearAlgebraVector GeometrySphere::calculatePointFromDistanceOnSurface(const LinearAlgebraVector &startingPoint, double x, double y) const {
-    double circumference = 2*m_radius*M_PI;
-    x = std::fmod(x, circumference);
-    y = std::fmod(y, circumference);
-    auto startingPointInSphereCoordiantes = startingPoint - m_center;
-    auto phiOld = acos(startingPointInSphereCoordiantes.getX()/m_radius);
-
-    if (startingPointInSphereCoordiantes.getY() < 0) {
-        phiOld = (-1) * phiOld;
-    }
-
-    auto additionalPhi = x/m_radius;
-    auto phiNew = phiOld + additionalPhi;
-    auto intermediatePoint = LinearAlgebraVector(m_radius*cos(phiNew), m_radius*sin(phiNew), startingPointInSphereCoordiantes.getZ());
-
-    auto thetaOld = acos(intermediatePoint.getY()/m_radius);
-
-    if (intermediatePoint.getZ() < 0) {
-        thetaOld = (-1) * thetaOld;
-    }
-
-    auto additionalTheta = y/m_radius;
-    auto thetaNew = thetaOld + additionalTheta;
-    auto resultInSphereCoordinates = LinearAlgebraVector(intermediatePoint.getX(), m_radius*cos(thetaNew), m_radius*sin(thetaNew));
-    return resultInSphereCoordinates + m_center;
-}
--- src/math/GeometrySphereTest.h
+++ src/math/GeometrySphereTest.h
@@ -8,21 +8,11 @@ class GeometrySphereTest : public CppUnit::TestFixture {
         CPPUNIT_TEST_SUITE(GeometrySphereTest);
         CPPUNIT_TEST(calculatePoint_anglePiHalfAndPiHalf_correctPoint);
         CPPUNIT_TEST(calculatePoint_anglePiAndPiHalf_correctPoint);
-        CPPUNIT_TEST(calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircle_northPole);
-        CPPUNIT_TEST(calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircleAndHalfRadius_northPole);
-        CPPUNIT_TEST(calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircleReverse_southPole);
-        CPPUNIT_TEST(calculatePointFromDistanceOnSurface_noMovementAtAll_samePoint);
-        CPPUNIT_TEST(calculatePointFromDistanceOnSurface_twiceCompleteCircle_samePoint);
         CPPUNIT_TEST_SUITE_END();
 
     public:
         void calculatePoint_anglePiHalfAndPiHalf_correctPoint();
         void calculatePoint_anglePiAndPiHalf_correctPoint();
-        void calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircle_northPole();
-        void calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircleAndHalfRadius_northPole();
-        void calculatePointFromDistanceOnSurface_equatorAndQuarterCircleAndQuarterCircleReverse_southPole();
-        void calculatePointFromDistanceOnSurface_noMovementAtAll_samePoint();
-        void calculatePointFromDistanceOnSurface_twiceCompleteCircle_samePoint();
 
     private:
 };
