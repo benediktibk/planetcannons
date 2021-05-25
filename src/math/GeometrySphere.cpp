89b43ecc66a99e87d28c1a0f8d9f2cbf86704f56
Benedikt Schmidt <benediktibk@gmail.com>
2021-05-25 18:44:50 +0200

    implement depth 0 of sphere approximation

--- src/math/GeometrySphere.cpp
+++ src/math/GeometrySphere.cpp
@@ -12,3 +12,49 @@ LinearAlgebraVector GeometrySphere::calculatePoint(double theta, double phi) con
     auto z = m_center.getZ() + m_radius * cos(theta);
     return LinearAlgebraVector(x, y, z);
 }
+
+std::vector<GeometryTriangle> GeometrySphere::approximateWithTriangles(unsigned int searchDepth) const {
+    std::vector<GeometryTriangle> result;
+    const double a = sqrt(3)*m_radius;
+    const double h = a * sin(M_PI/3);
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector((-1)*a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 0, 1)));
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector(0, 0, 1),
+        LinearAlgebraVector(a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 1, 0)));
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector((-1)*a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 0, 1),
+        LinearAlgebraVector(0, 1, 0)));
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector((-1)*a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 0, -1)));
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector(0, 0, -1),
+        LinearAlgebraVector(a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 1, 0)));
+
+    result.push_back(GeometryTriangle(
+        LinearAlgebraVector((-1)*a/2, (-1)*(h - 1), 0),
+        LinearAlgebraVector(0, 0, -1),
+        LinearAlgebraVector(0, 1, 0)));
+
+    return approximateWithTrianglesRecursive(result, searchDepth);
+}
+
+std::vector<GeometryTriangle> GeometrySphere::approximateWithTrianglesRecursive(const std::vector<GeometryTriangle> &triangles, unsigned int searchDepth) const {
+    if (searchDepth == 0) {
+        return triangles;
+    }
+
+    return triangles;
+}
--- src/math/GeometrySphere.h
+++ src/math/GeometrySphere.h
@@ -2,12 +2,18 @@
 #define PLANETCANNONS_GEOMETRYSPHERE_H
 
 #include "LinearAlgebraVector.h"
+#include "GeometryTriangle.h"
+#include <vector>
 
 class GeometrySphere {
     public:
         GeometrySphere(const LinearAlgebraVector &center, double radius);
 
         LinearAlgebraVector calculatePoint(double theta, double phi) const;
+        std::vector<GeometryTriangle> approximateWithTriangles(unsigned int searchDepth) const;
+
+    private:
+        std::vector<GeometryTriangle> approximateWithTrianglesRecursive(const std::vector<GeometryTriangle> &triangles, unsigned int searchDepth) const;
 
     private:
         LinearAlgebraVector m_center;
